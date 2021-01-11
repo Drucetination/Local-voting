9dad58dfcb1a3a756e5db966b221ccff66033848
Drucetination <st050025@student.spbu.ru>
2021-01-11 22:14:52 +0300

    Local voting protocol fixed Noise added

--- src/main/java/ru/spbu/lv/FindAverageBehaviour.java
+++ src/main/java/ru/spbu/lv/FindAverageBehaviour.java
@@ -1,6 +1,7 @@
 package ru.spbu.lv;
 
 import java.util.Objects;
+import java.util.Random;
 
 import jade.core.AID;
 import jade.core.behaviours.TickerBehaviour;
@@ -11,7 +12,7 @@ import jade.lang.acl.ACLMessage;
 public class FindAverageBehaviour extends TickerBehaviour {
     private final DefaultAgent agent;
     private int currentStep;
-    private final int MAX_STEPS = 40;
+    private final int MAX_STEPS = 20;
 
     FindAverageBehaviour(DefaultAgent agent, long period) {
         super(agent, period);
@@ -24,31 +25,45 @@ public class FindAverageBehaviour extends TickerBehaviour {
     @Override
     public void onTick() {
 
-        System.out.println(this.getTickCount());
+        int neighbors_n = this.agent.getLinkedAgents().length;
 
-        int neighbours_n = this.agent.getLinkedAgents().length;
+        // set noise
+        Random noise_randomizer = new Random();
+        float[] noise = new float[neighbors_n];
+        for (int i=0; i<noise.length; i++)
+            noise[i] = (float) noise_randomizer.nextGaussian() / 100f;
+
+        // set presence / absence of communication with neighbors
+        boolean[] alive_links = new boolean[neighbors_n];
+        for (int i =0; i < alive_links.length; i++)
+            alive_links[i] = noise_randomizer.nextBoolean();
 
         if (currentStep < MAX_STEPS) {
+
+            // messages sending
             ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
             msg.setSender(this.agent.getAID());
-            for(int i=0; i < neighbours_n; i++)
-                msg.addReceiver(new AID(this.agent.getLinkedAgents()[i], AID.ISLOCALNAME));
-
-            msg.setContent(Objects.toString(this.agent.getNumber()));
-            this.agent.send(msg);
-
-            ACLMessage[] received_messages = new ACLMessage[this.agent.getCurQueueSize()];
-            for (int i=0; i<received_messages.length; i++){
-                received_messages[i] = this.agent.blockingReceive();
+            for(int i=0; i < neighbors_n; i++) {
+                if (alive_links[i])
+                    msg.addReceiver(new AID(this.agent.getLinkedAgents()[i], AID.ISLOCALNAME));
+                    msg.setContent(Objects.toString(this.agent.getNumber() + noise[i]));
+                    this.agent.send(msg);
             }
-            float a = 1f / (1f + (float) received_messages.length);
 
+            // messages receiving and neighbors' values sum calculation
             float sum_neighbours = 0;
-            for (ACLMessage received_message : received_messages) {
+            int amount = 0;
+            while (this.agent.getCurQueueSize() > 0 && amount < 2){
+                ACLMessage received_message = this.agent.receive();
                 sum_neighbours += Float.parseFloat(received_message.getContent());
+                amount++;
             }
 
-            float current_number = a * (this.agent.getNumber() + sum_neighbours);
+            // step size
+            float a = 1f / (1f + (float) neighbors_n);
+
+            // value update
+            float current_number = this.agent.getNumber() + a * (sum_neighbours -  amount * this.agent.getNumber());
             this.agent.setNumber(current_number);
 
             currentStep++;
